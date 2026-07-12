d135646ab708c4f29672bf2ef0843a1c972fdc60
Darkavenger59 <97632342+Darkavenger59@users.noreply.github.com>
2026-07-12 14:25:38 +0200

    Change CACHE_NAME to 'b-strong-v5.4'
    Updated CACHE_NAME to force cache refresh.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'b-strong-v5.3'; // J'ai changé le nom pour forcer la mise à jour
+const CACHE_NAME = 'b-strong-v5.4'; // J'ai changé le nom pour forcer la mise à jour
 const ASSETS = [
   './',
   './index.html',
