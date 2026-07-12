897e3aeaf8af009da4f6f19742a456ccf11402b0
Darkavenger59 <97632342+Darkavenger59@users.noreply.github.com>
2026-07-12 14:45:49 +0200

    Update CACHE_NAME to force service worker update
    Changed CACHE_NAME to force update of the service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'b-strong-v7'; // J'ai changé le nom pour forcer la mise à jour
+const CACHE_NAME = 'b-strong-v7.1'; // J'ai changé le nom pour forcer la mise à jour
 const ASSETS = [
   './',
   './index.html',
