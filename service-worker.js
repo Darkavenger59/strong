1f3c7ecb46ef5a09a37e332b4585aa391ed4e98c
Darkavenger59 <97632342+Darkavenger59@users.noreply.github.com>
2026-07-14 17:44:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'b-strong-v8.5'; // J'ai changé le nom pour forcer la mise à jour
+const CACHE_NAME = 'b-strong-v8.6'; // J'ai changé le nom pour forcer la mise à jour
 const ASSETS = [
   './',
   './index.html',
